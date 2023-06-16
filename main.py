ca0c0f573721577b43b1b8873711456899d876ef
Puglord6383 <119639039+Puglord6383@users.noreply.github.com>
2023-06-15 23:37:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-auth_key = "your_key" # replace your_key with your api key
+auth_key = "your_key" # replace your_key wih your api key
 club_tag = "your_club_tag" # replace your_club_tag with your club tag (# instead of %23 is ok)
 
 import time
@@ -7,27 +7,30 @@ import requests
 import pandas as pd
 import datetime
 
-# Adding 'Bearer ' before the key and replacing '#' in the club tag
 headers = {
     "Accept": "application/json",
     "Authorization": "Bearer " + auth_key,
 }
 
+# Replace '#' with '%23' in the club tag
+club_tag = club_tag.replace('#', '%23')
+
 # Insert the club tag into the URL
-url_club = f"https://api.brawlstars.com/v1/clubs/{club_tag.replace('#', '%23')}/members"
-
-
-headers = {
-    "Accept": "application/json",
-    "Authorization": auth_key,
-}
-
+url_club = f"https://api.brawlstars.com/v1/clubs/{club_tag}/members"
 
 # Fetch club members
+print('Fetching club members...')
 time.sleep(1)
 response_club = requests.get(url_club, headers=headers)
 data_club = response_club.json()
 
+# Handle the response for club members
+if response_club.status_code == 200:
+    print(f'Successfully fetched club members: {len(data_club["items"])} members found.')
+else:
+    print(f'Failed to fetch club members: {response_club.status_code} - {data_club["message"]}')
+
+# Generate club members dictionary
 club_members = {member['tag'].replace('#', '%23'): member['name'] for member in data_club['items']}
 
 # Try to load existing player stats, last processed battleTime, and the week counter from files
@@ -52,9 +55,17 @@ for player_id, player_name in club_members.items():
     url_battlelog = f"https://api.brawlstars.com/v1/players/{player_id}/battlelog"   # player battle logs
     time.sleep(1)
     response_battlelog = requests.get(url_battlelog, headers=headers)
-    assert response_battlelog.status_code == 200, f"Failed to fetch battlelog for player {player_id}"
     
+    if response_battlelog.status_code == 200:
+        print(f'Successfully fetched data for player {player_name}')
+    else:
+        data_battlelog = response_battlelog.json()
+        print(f'Failed to fetch data for player {player_name}: {response_battlelog.status_code} - {data_battlelog["message"]}')
+        continue
+
     data_battlelog = response_battlelog.json()
+    
+    initial_trophies = player_stats[player_name]['Trophies'] if player_name in player_stats else 0
 
     if player_name not in player_stats:
         player_stats[player_name] = {'Trophies': 0, 'Wins': 0, 'Losses': 0, 'Team': 0, 'Solo': 0, 'Team Wins': 0, 'Team Losses': 0, 'Solo Wins': 0, 'Solo Losses': 0}
@@ -84,6 +95,8 @@ for player_id, player_name in club_members.items():
                 
             if battle_time > most_recent_battle_time:
                 most_recent_battle_time = battle_time
+    trophies_added = player_stats[player_name]['Trophies'] - initial_trophies
+    print(f"{player_name} added {trophies_added} trophies.")
 
 # Save player stats and last processed battleTime to files
 df = pd.DataFrame(player_stats).T
